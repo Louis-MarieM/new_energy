258e34f89a2cad8dc54cf8da32b28f9c89938b1a
Louis-MarieM <lmmaitrepierre@gmail.com>
2022-11-26 17:29:17 +0100

    [feature/loginCheck] modif login totalEerngies et views

--- new_energy/scraping/views.py
+++ new_energy/scraping/views.py
@@ -6,35 +6,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from scraping.forms import LoginForm
+from scraping.services.fournisseurs.totalenergies import FournisseurTotalEnergies as totalEnergies
 
 def launcher(request):
     if request.method == 'POST':
         global wait, browser, Link
         form = LoginForm(request.POST)
+        
         if form.is_valid():
-            options = webdriver.ChromeOptions()
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--headless")
-            options.add_argument("--disable-gpu")
-            browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-            wait = WebDriverWait(browser, 600)
-            print("URL: ",form.cleaned_data['website'])
-            browser.get(form.cleaned_data['website'])
-            browser.maximize_window()
-            print("page ouverte")
-            time.sleep(2)
-            input_identifiant = browser.find_element("xpath", '//*[@id="tx_deauthentification_login"]')
-            input_pwd = browser.find_element("xpath", '//*[@id="tx_deauthentification_password"]')
-            input_submit = browser.find_element("xpath", '//*[@id="tx_deauthentification_mdp_oublie"]')
-
-            input_identifiant.click()
-            input_identifiant.send_keys(form.cleaned_data['identifier'])
-            time.sleep(1)
-            input_pwd.click()
-            input_pwd.send_keys(form.cleaned_data['password'])
-            time.sleep(1)
-            input_submit.click()
+            if (totalEnergies.energy_login(form.cleaned_data['identifier'], form.cleaned_data['password'], form.cleaned_data['website'])):
+                return render(request, 'scraping/launcher.html', {'form': form})
+            else:
+                form = LoginForm()
     else :
         form = LoginForm()
     return render(request, 'scraping/launcher.html', {'form': form})
